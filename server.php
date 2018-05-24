d69d8b43f1aae87a030db986faf6d80f89e6a3bb
Nardberjean <Nardberjean@users.noreply.github.com>
2018-05-24 18:18:53 -0400

    Update server.php

--- server.php
+++ server.php
@@ -10,10 +10,10 @@ $server->setBaseUri('/');
 $server->addPlugin(new DAV\Locks\Plugin(new DAV\Locks\Backend\File('data/locks')));
 $server->addPlugin(new DAV\Browser\Plugin());
 
-# $authBackend = new Auth\Backend\File('htdigest');
-# $authBackend->setRealm('SabreDAV');
-# $server->addPlugin(new Auth\Plugin($authBackend));
-$server->addPlugin(((new Auth\Backend\File('htdigest'))->setRealm('SabreDAV')));
+$authBackend = new Auth\Backend\File('htdigest');
+$authBackend->setRealm('SabreDAV');
+$server->addPlugin(new Auth\Plugin($authBackend));
+# $server->addPlugin(((new Auth\Backend\File('htdigest'))->setRealm('SabreDAV'))); # Seems not to work.
 
 $server->exec();
 
