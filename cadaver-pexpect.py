3db9d9bf1c95ef01f6b88d480f81319d9bbec480
Nardberjean <Nardberjean@users.noreply.github.com>
2018-05-24 19:59:07 -0400

    Update cadaver-pexpect.py

--- cadaver-pexpect.py
+++ cadaver-pexpect.py
@@ -42,8 +42,8 @@ child = pexpect.spawnu('cadaver http://localhost:8000')
 # child.sendline('user')
 # child.expect('dav:/> ')
 # print("Escape character is '^]'.\n")
-sys.stdout.write (child.after)
-sys.stdout.flush()
+# sys.stdout.write (child.after)
+# sys.stdout.flush()
 # child.interact() # Escape character defaults to ^]
 # At this point this script blocks until the user presses the escape character
 # or until the child exits. The human user and the child should be talking
